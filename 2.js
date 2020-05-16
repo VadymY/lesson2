30330fe3bc2398cd944629ce79c02d64dda30aac
Vadym Yashchenko <vadym.yashchenko@gmail.com>
2020-05-16 14:47:37 +0300

    lesson-2 tasks is finish

---
+++ 2.js
@@ -0,0 +1,23 @@
+// Перепишите `if..else` с использованием нескольких операторов `?`. 
+// Для читаемости — оформляйте код в несколько строк.
+
+////////////////// Задание //////////////////
+// var message;
+
+// if (login == 'Pitter') {
+//   message = 'Hi';
+// } else if (login == 'Owner') {
+//   message = 'Hello';
+// } else if (login == '') {
+//   message = 'unknown';
+// } else {
+//   message = '';
+// }
+
+////////////////// Решение //////////////////
+var login = ''; //  just for compiling
+var message;
+
+message = (login == 'Pitter') ? 'Hi' :
+            (login == 'Owner') ? 'Hello' :
+                (login == '') ? 'unknown' : '';
